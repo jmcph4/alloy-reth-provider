afc055f2bc2fcbcab2d15d29c5883852ebd34ac2
cakevm <cakevm@proton.me>
2025-04-08 15:40:43 +0200

    Always return zero for StateRootProvider fn's

--- src/state_provider/impls/state_root_provider.rs
+++ src/state_provider/impls/state_root_provider.rs
@@ -6,25 +6,28 @@ use reth_errors::ProviderResult;
 use reth_provider::StateRootProvider;
 use reth_trie::updates::TrieUpdates;
 use reth_trie::{HashedPostState, TrieInput};
+use tracing::warn;
 
 impl<N, P> StateRootProvider for AlloyRethStateProvider<N, P>
 where
     N: Network,
     P: Provider<N> + Clone,
 {
-    fn state_root(&self, _hashed_state: HashedPostState) -> ProviderResult<B256> {
-        todo!()
+    fn state_root(&self, hashed_state: HashedPostState) -> ProviderResult<B256> {
+        self.state_root_from_nodes(TrieInput::from_state(hashed_state))
     }
 
     fn state_root_from_nodes(&self, _input: TrieInput) -> ProviderResult<B256> {
-        todo!()
+        warn!("state_root_from_nodes is not implemented and will return zero");
+        Ok(B256::ZERO)
     }
 
-    fn state_root_with_updates(&self, _hashed_state: HashedPostState) -> ProviderResult<(B256, TrieUpdates)> {
-        todo!()
+    fn state_root_with_updates(&self, hashed_state: HashedPostState) -> ProviderResult<(B256, TrieUpdates)> {
+        self.state_root_from_nodes_with_updates(TrieInput::from_state(hashed_state))
     }
 
     fn state_root_from_nodes_with_updates(&self, _input: TrieInput) -> ProviderResult<(B256, TrieUpdates)> {
-        todo!()
+        warn!("state_root_from_nodes_with_updates is not implemented and will return zero");
+        Ok((B256::ZERO, TrieUpdates::default()))
     }
 }
