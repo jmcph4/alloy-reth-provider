b368a5e7d74e67c7d056f93c7c7ee56db16d0826
cakevm <cakevm@proton.me>
2025-04-08 13:23:51 +0200

    Add impl for hashed_post_state

--- src/state_provider/impls/hashed_post_state_provider.rs
+++ src/state_provider/impls/hashed_post_state_provider.rs
@@ -2,7 +2,7 @@ use crate::AlloyRethStateProvider;
 use alloy_network::Network;
 use alloy_provider::Provider;
 use reth_provider::HashedPostStateProvider;
-use reth_trie::HashedPostState;
+use reth_trie::{HashedPostState, KeccakKeyHasher};
 use revm_database::BundleState;
 
 impl<N, P> HashedPostStateProvider for AlloyRethStateProvider<N, P>
@@ -10,7 +10,7 @@ where
     N: Network,
     P: Clone + Provider<N>,
 {
-    fn hashed_post_state(&self, _bundle_state: &BundleState) -> HashedPostState {
-        todo!()
+    fn hashed_post_state(&self, bundle_state: &BundleState) -> HashedPostState {
+        HashedPostState::from_bundle_state::<KeccakKeyHasher>(bundle_state.state())
     }
 }
